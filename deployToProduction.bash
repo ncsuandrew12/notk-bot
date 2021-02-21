e018cd520be87a1631b40a5a6309161135a80a06
Andrew Felsher <ncsuandrew12@gmail.com>
2021-02-21 13:22:17 -0500

    Created test deployment script.

---
+++ deployToProduction.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+TARGET="/production/notk-bot"
+
+tag=`git tag --points-at HEAD`
+
+if [[ `echo "$tag" | wc -w` -eq 0 ]]; then
+    >&2 echo "ERROR: The current commit is not tagged."
+    exit 1
+fi
+
+echo "Deploying to production"
+
+cp -vf main.py "${TARGET}/"
+cp -vf discord.token "${TARGET}/"
+cp -vf config.json "${TARGET}/"
