27cfb2c37f8e18984f0165709888b2f065153ee3
Andrew Felsher <ncsuandrew12@gmail.com>
2021-02-21 13:22:17 -0500

    Created test deployment script.

---
+++ deployToTest.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+TARGET="/test/notk-bot"
+
+echo "Deploying to test"
+
+cp -vf main.py "${TARGET}/"
+cp -vf discord-test.token "${TARGET}/discord.token"
+cp -vf config-test.json "${TARGET}/config.json"
