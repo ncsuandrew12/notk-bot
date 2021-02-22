f466d1c6e72784e847485e468b3c5c25ac21f606
Andrew Felsher <ncsuandrew12@gmail.com>
2021-02-22 17:21:21 -0500

    Updated release/deployment scripts. Switched startup code to update old instructional message.

--- bin/deployToProduction.bash
+++ bin/deployToProduction.bash
@@ -14,6 +14,8 @@ fi
 
 echo "Deploying to production"
 
+cp -vf ${ROOT_DIR}/Release_Notes.md ${TARGET}/
+
 cp -vf ${ROOT_DIR}/src/*.py ${TARGET}/
 
 mkdir -p ${TARGET}/cfg/
--- bin/release.bash
+++ bin/release.bash
@@ -13,6 +13,8 @@ if [ "$1" == "--dry-run" ]; then
     dryRun=1
 fi
 
+majorRelease=0
+
 if [[ `git status --short | wc -l` -ne 0 ]]; then
     >&2 echo "ERROR: Uncommitted files:"
     git status
@@ -24,17 +26,28 @@ if [ $dryRun -eq 0 ]; then
     git push
 fi
 
+if [[ ! -f release_notes ]]; then
+    >&2 echo "Missing release notes!"
+    exit 3
+fi
+
 major=0
-minor=1;
+minor=0;
 if [ `git tag --list | wc -l` -gt 0 ]; then
+    # TODO Generalize to any number of digits
     latestVersion=`git tag --list | sed -r 's/\.([0-9])$/.0\1/g' | sed -r 's/\.([0-9][0-9])$/.0\1/g' | sort | tail -1`
     latestMajor=$(sed s'/\.[^.]*$//' <<< $latestVersion)
     latestMinor=$(sed s'/[^.]*\.//' <<< $latestVersion)
-    latestMajor=$(sed -r 's/^0+([0-9])/\1/' <<< $latestMajor)
-    latestMinor=$(sed -r 's/^0+([0-9])/\1/' <<< $latestMinor)
+    latestMajor=$(sed -r 's/^0+([1-9][0-9]*)/\1/' <<< $latestMajor)
+    latestMinor=$(sed -r 's/^0+([1-9][0-9]*)/\1/' <<< $latestMinor)
 
     major=$((latestMajor))
-    minor=$((latestMinor+1))
+    if [ $majorRelease -eq 1 ]; then
+        major=$((major+1))
+        minor=0
+    else
+        minor=$((latestMinor+1))
+    fi
 fi;
 
 newVersion="${major}.${minor}"
@@ -48,6 +61,24 @@ if [[ "$doRelease" != "y" ]]; then
     exit 2
 fi
 if [ $dryRun -eq 0 ]; then
+    echo "Creating release branch"
+    if [ $majorRelease -eq 1 ]; then
+        git checkout -b release/${major}
+    fi
+
+    echo "Creating release directory."
+    mkdir -p releases/${tagLabel}
+
+    echo "Moving release notes."
+    cp release_notes Release_Notes.md
+    mv release_notes releases/${tagLabel}/Release_Notes.md
+
+    echo "Committing changes."
+    git add releases/${tagLabel}
+    git commit -m "Prepping release: $tagLabel"
+    git push
+
+    echo "Labelling release."
     git tag -a "$tagLabel" -m "Tagging $tagLabel"
 
     echo "Pushing tags."
