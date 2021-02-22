ddb56b39926f18163eb20cf6fe7e6b081b09ddf6
Andrew Felsher <ncsuandrew12@gmail.com>
2021-02-22 17:30:36 -0500

    Moved command processing into GuildBot.

--- src/NotkBot.py
+++ src/NotkBot.py
@@ -6,7 +6,6 @@ import discord
 # notk-bot
 import Error
 import Logging as log
-import LoggingDiscord as dlog
 
 from Config import cfg
 from GuildBot import GuildBot
@@ -45,51 +44,7 @@ class NotkBot:
           "andrewf#6219"))
 
       guildBot = self.guildBots[ctx.guild.id]
-      dlog.debug(ctx, "Processing command: `{} {}`".format(cmd, " ".join(args)))
-
-      members = []
-      memberNames = []
-      resolved = []
-      if cmd in [ cfg.cCommandJoin, cfg.cCommandLeave]:
-        if len(args) > 0:
-          userIDs = {}
-          userNames = []
-          for arg in args:
-            if arg.startswith('<@') & arg.endswith('>'):
-              userID = arg[2:-1]
-              while userID.startswith('!') | userID.startswith('&'):
-                userID = userID[1:len(userID)]
-              userIDs[userID] = arg
-            else:
-              userNames.append(arg)
-          for userID in userIDs:
-            try:
-              member = await ctx.guild.fetch_member(userID)
-            except Exception as e:
-              dlog.serverWarn(ctx, "userID `{}`: {}".format(userID, str(e)))
-            except:
-              dlog.serverWarn(ctx, "userID `{}`: {}".format(userID, str(sys.exc_info()[0])))
-            else:
-              if member.name not in memberNames:
-                resolved.append(userIDs[userID])
-                members.append(member)
-                memberNames.append(member.name)
-        else:
-          member = await ctx.guild.fetch_member(ctx.author.id)
-          members = [member]
-          memberNames = [member.name]
-        missing = set(args) - set(resolved)
-        if (len(missing) > 0):
-          await dlog.warn(guildBot, ctx, "Could not find `{}` members: `{}`!".format(ctx.guild.name, "`, `".join(missing)))
-
-      if cmd == cfg.cCommandJoin:
-        await guildBot.addAmongUsPlayer(ctx, members)
-      elif cmd == cfg.cCommandLeave:
-        await guildBot.removeAmongUsPlayer(ctx, members)
-      elif cmd == cfg.cCommandNewGame:
-        await guildBot.notifyAmongUsGame(ctx, ctx.message.channel, args[0])
-      else:
-        await Error.dErr(guildBot, ctx, "Invalid command `{}`.".format(cmd))
+      await guildBot.Command(ctx, cmd, *args)
     except NotkException as e:
       # This error will have already been logged
       return
