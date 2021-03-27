ae378be5567c88d14b0aee133469569d25f06e4d
Andrew Felsher <ncsuandrew12@gmail.com>
2021-03-27 02:14:38 -0400

    Release script fixes.

--- bin/releaseToProduction.bash
+++ bin/releaseToProduction.bash
@@ -7,24 +7,32 @@ source "${DIR}/common.bash"
 
 dryRun=0
 
+args=$@
+
 while (( "$#" )); do
     case "$1" in
         -d|--dry-run)
             dryRun=1
             shift
             ;;
-        # -*|--*=) # unsupported flags
-        #     >&2 echo "ERROR: Unsupported parameter $1"
-        #     exit $ERR_BAD_PARAMETER
-        #     ;;
-        # *) # preserve positional arguments
-        #     >&2 echo "ERROR: Unexpected argument $1"
-        #     exit $ERR_BAD_ARGUMENT
-        #     ;;
+        --major-release)
+            shift
+            ;;
+        --minor-release)
+            shift
+            ;;
+        -*|--*=) # unsupported flags
+            >&2 echo "ERROR: Unsupported parameter $1"
+            exit $ERR_BAD_PARAMETER
+            ;;
+        *) # preserve positional arguments
+            >&2 echo "ERROR: Unexpected argument $1"
+            exit $ERR_BAD_ARGUMENT
+            ;;
     esac
 done
 
-${ROOT_DIR}/bin/createRelease.bash "$@"
+${ROOT_DIR}/bin/createRelease.bash "$args"
 
 if [ $dryRun -eq 0 ]; then
     ${ROOT_DIR}/bin/deployToProduction.bash
