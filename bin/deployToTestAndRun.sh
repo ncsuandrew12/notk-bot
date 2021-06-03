25cbd0b41fce77e23d456440550d00efe0a84f23
Andrew Felsher <ncsuandrew12@gmail.com>
2021-06-02 22:47:55 -0400

    Added script for easily deploying and running a test instance.

---
+++ bin/deployToTestAndRun.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source "${DIR}/common.bash"
+
+set -e
+
+${ROOT_DIR}/bin/deploy.bash test
+${ROOT_DIR}/bin/startServerTest.bash
