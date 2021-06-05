0bb4afe703764058986002ad82141884d5c52ebe
Andrew Felsher <ncsuandrew12@gmail.com>
2021-06-05 00:51:16 -0400

    Minor test script fix.

--- bin/test.bash
+++ bin/test.bash
@@ -5,7 +5,7 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "${DIR}/common.bash"
 
-${ROOT_DIR}/bin/deploy.bash test
+${ROOT_DIR}/bin/deploy.bash --target test
 
 deploymentJson=$(jq -r "." ${ROOT_DIR}/cfg/test/deploy.json)
 targetDir=/home/`whoami`/$(jq -r ".dir" <<< ${deploymentJson})
